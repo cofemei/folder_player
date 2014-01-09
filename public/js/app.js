16293fa7a9a090e015c79e5a99f65149584dd3ca
9gmei <cofemei@gmail.com>
2014-01-09 13:59:06 +0800

    init

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module("app", ['ngRoute']);
+
+app.config(function($routeProvider) {
+
+  $routeProvider.when('/photo', {
+    templateUrl: 'photo.html',
+    controller: 'HelloCtrl'
+  });
+
+  $routeProvider.otherwise({ redirectTo: '/photo' });
+});
+
+
+app.controller('HelloCtrl', ['$scope', '$http', function($scope, $http) {
+	// $scope.greeting = 'Hola!';
+	// $scope.array = ['1', '2'];
+	$http.get("/vol").success(function (data) {
+    	$scope.array = data;
+  	});
+
+  	$scope.open = function(item) {
+    	$http.get("/list/?query="+item).success(function (data) {
+    		console.log(data);
+    		$scope.imgArray = data;
+  		});
+  	};
+}]);
+
